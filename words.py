dc20ee56901d15c7accd4d9a7f3cb22ecebe07db
blazingakash1005 <46522898+akash100507@users.noreply.github.com>
2021-02-10 15:03:25 +0530

    Update words.py

--- words.py
+++ words.py
@@ -1,18 +1,22 @@
 import requests
 import random
-url = "https://raw.githubusercontent.com/akash100507/lists/main/words.txt"
-
-response = requests.get(url, stream=True)
-
-l = []
-
-for i in response.iter_lines():
-  t = i.decode("utf-8")
-  l.append(t)
   
-
-#print(l)
-print(len(l))
-
-for w in random.sample(l,10):
-  print(w)
+def display(link):
+  response = requests.get(link, stream=True)
+  l = []
+  for i in response.iter_lines():
+    t = i.decode("utf-8")
+    l.append(t)
+  print(len(l))
+  for w in random.sample(l,10):
+    print(w)
+  
+  
+  
+if __name__ == "__main__":
+  url1 = "https://raw.githubusercontent.com/akash100507/lists/main/words1.txt"
+  print("List1")
+  display(url1)
+  url2 = "https://raw.githubusercontent.com/akash100507/lists/main/words2.txt"
+  print("List2")
+  display(url2)
