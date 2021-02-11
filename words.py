e4392aedd69b25dea5802e62a40849d59c3f2fa9
blazingakash1005 <46522898+akash100507@users.noreply.github.com>
2021-02-11 20:31:26 +0530

    Update words.py

--- words.py
+++ words.py
@@ -7,8 +7,12 @@ def display(link):
   for i in response.iter_lines():
     t = i.decode("utf-8")
     l.append(t)
-  print(len(l))
-  for w in random.sample(l,10):
+  u = len(l)
+  if (u>=335):
+    m = 30
+  else:
+    m = 15
+  for w in random.sample(l,m):
     print(w)
   
   
@@ -20,12 +24,12 @@ if __name__ == "__main__":
   url2 = "https://raw.githubusercontent.com/akash100507/lists/master/words2.txt"
   print("List2")
   display(url2)
-  url3 = "https://raw.githubusercontent.com/akash100507/lists/master/word_undo.txt"
+  url3 = "https://raw.githubusercontent.com/akash100507/lists/master/words3.txt"
   print("List3")
   display(url3)
-  url4 = "https://raw.githubusercontent.com/akash100507/lists/master/words3.txt"
+  url4 = "https://raw.githubusercontent.com/akash100507/lists/master/words4.txt"
   print("List4")
   display(url4)
-  url5 = "https://raw.githubusercontent.com/akash100507/lists/master/words4.txt"
+  url5 = "https://raw.githubusercontent.com/akash100507/lists/master/word_undo.txt"
   print("List5")
   display(url5)
