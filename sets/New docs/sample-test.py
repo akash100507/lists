e46cdf42a78bde96d2471ba98b236ae68c4e768d
akash1005 <skynet@skynet.com>
2021-07-12 02:37:01 +0530

    test

---
+++ sets/New docs/sample-test.py
@@ -0,0 +1,20 @@
+import requests
+import random
+  
+def display(link):
+  response = requests.get(link, stream=True)
+  l = []
+  for i in response.iter_lines():
+    t = i.decode("utf-8")
+    l.append(t)
+  u = len(l)
+  m = 50
+  for w in random.sample(l,m):
+    print(w)
+    
+if __name__ == "__main__":
+  
+  url = "https://raw.githubusercontent.com/akash100507/lists/master/sets/lists"+str(j)+".txt"
+# print("List "+str(j))
+  display(url)
+#   #print("\n\n\n------------------------------------\n\n")
