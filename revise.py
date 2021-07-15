0f254a89ca62c8a69b87efbbf1587ecd5cc952df
akash100507 <46522898+akash100507@users.noreply.github.com>
2021-07-16 00:10:15 +0530

    Update revise.py

--- revise.py
+++ revise.py
@@ -8,13 +8,13 @@ def display(link):
     t = i.decode("utf-8")
     l.append(t)
   u = len(l)
-  m = 50
+  m = 75
   for w in random.sample(l,m):
     print(w)
     
 if __name__ == "__main__":
   
-  for j in range(1,3):
+  for j in range(1,4):
     url = "https://raw.githubusercontent.com/akash100507/lists/master/sets/lists"+str(j)+".txt"
     print("List "+str(j))
     display(url)
